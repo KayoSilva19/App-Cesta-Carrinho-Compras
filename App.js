7595b71fe3e8c87f4bc125db80ad0043aa43e04c
KayoSilva19 <Kayohenriquesilva19@gmail.com>
2021-11-30 16:21:12 -0300

    app-Carrinho-Compras

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StatusBar, SafeAreaView,  } from 'react-native';
+import Cesta from './src/pages/Cestas';
+
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
+import mock from './src/mocks/cesta';
+
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  });
+
+  if (!fontCarregada) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="light" backgroundColor="#DDEBE0" />
+      <Cesta {...mock} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
